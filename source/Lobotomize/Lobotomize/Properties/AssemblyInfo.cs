b35f4d0316301a78a0eeba485479df153b45a70b
DESKTOP-CSOMB5I\Stickie <pieterdestickere@gmail.com>
2018-09-27 00:46:29 +0200

    - Transfer from laptop to desktop. (Unfinished)

---
+++ source/Lobotomize/Lobotomize/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lobotomize")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lobotomize")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7153b88e-f761-45c7-9010-4cd4aab13328")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ source/Lobotomize/Lobotomize/Test.cs
@@ -0,0 +1,39 @@
+﻿using RimWorld;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace Lobotomize
+{
+    public class Test : HediffComp
+    {
+        /*
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            if (Pawn != null)
+            {
+                if (Pawn.RaceProps != null)
+                {
+                    if (Pawn.RaceProps.IsFlesh)
+                    {
+                        PlsLogMessage("OMGYES!");
+                    }
+                }
+            }
+        }
+
+        public override string CompDebugString()
+        {
+            return "Testing123";
+        }
+
+        public static readonly TraitDef TheTrait = TraitDefOf.Psychopath;
+        private static void PlsLogMessage(string _m)
+        {
+
+            //pawn.story.traits.GainTrait(new Trait(traitDef, traitDegreeData.degree, false));
+            Log.Message("Hello man, this is the debug log working! (" + TheTrait.ToString() + ")" + _m, false);
+        }
+        */
+    }
+}
